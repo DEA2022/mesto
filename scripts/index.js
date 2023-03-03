0a7587afee68c77e0691c03671aea0eb03874c57
Ekaterina Danilova <ekat.sokolova2010@yandex.ru>
2023-03-03 21:34:25 +0300

    feat: Реализовать открытие и закрытие попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
+const popupElement = document.querySelector('.popup');
+const popupOpenBtnElement = document.querySelector('.profile__edit');
+const popupCloseBtnElement = popupElement.querySelector('.form__close');
 
+const openPopup = function () {
+  popupElement.classList.add('popup_opened');
+};
+
+const closePopup = function () {
+  popupElement.classList.remove('popup_opened');
+};
+
+popupOpenBtnElement.addEventListener('click', openPopup);
+
+popupCloseBtnElement.addEventListener('click', closePopup);
