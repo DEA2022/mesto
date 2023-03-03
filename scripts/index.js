5accdc6d9af71f7ac3b67d99b39ffefb77349ecd
Ekaterina Danilova <ekat.sokolova2010@yandex.ru>
2023-03-04 00:16:10 +0300

    feat: добавить редактирование формы

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// открытие и закрытие попапа
 const popupElement = document.querySelector('.popup');
 const popupOpenBtnElement = document.querySelector('.profile__edit');
 const popupCloseBtnElement = popupElement.querySelector('.form__close');
@@ -10,6 +11,31 @@ const closePopup = function () {
   popupElement.classList.remove('popup_opened');
 };
 
-popupOpenBtnElement.addEventListener('click', openPopup);
+const closePopupByClickOverlay = function (event) {
+  console.log(event.currentTarget, event.target);
+  if (event.currentTarget === event.target) {
+    closePopup();
+  }
+}
 
+popupOpenBtnElement.addEventListener('click', openPopup);
 popupCloseBtnElement.addEventListener('click', closePopup);
+popupElement.addEventListener('click', closePopupByClickOverlay);
+
+// редактирование формы
+const formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('.form__username');
+let jobInput = formElement.querySelector('.form__job-title');
+
+const handleFormSubmit = function (evt) {
+  evt.preventDefault();
+
+  let nameInputNewValue = document.querySelector('.profile__title');
+  let jobInputNewValue = document.querySelector('.profile__subtitle');
+  nameInputNewValue.textContent = nameInput.value;
+  jobInputNewValue.textContent = jobInput.value;
+
+  closePopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
