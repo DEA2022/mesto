0ea00bb363382b5a161c13032508ae421cbbc180
Ekaterina Danilova <ekat.sokolova2010@yandex.ru>
2023-03-04 22:31:44 +0300

    fix: исправить добавление значений при открытии попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,24 @@
-// открытие и закрытие попапа
 const popupElement = document.querySelector('.popup');
 const popupOpenBtnElement = document.querySelector('.profile__edit');
 const popupCloseBtnElement = popupElement.querySelector('.popup__close');
+const formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('.form__field_el_name');
+let jobInput = formElement.querySelector('.form__field_el_job');
+let nameInputNewValue = document.querySelector('.profile__title');
+let jobInputNewValue = document.querySelector('.profile__subtitle');
 
 const openPopup = function () {
+  nameInput.value = nameInputNewValue.textContent;
+  jobInput.value = jobInputNewValue.textContent;
   popupElement.classList.add('popup_opened');
 };
 
 const closePopup = function () {
   popupElement.classList.remove('popup_opened');
+
 };
 
 const closePopupByClickOverlay = function (event) {
-  console.log(event.currentTarget, event.target);
   if (event.currentTarget === event.target) {
     closePopup();
   }
@@ -22,19 +28,11 @@ popupOpenBtnElement.addEventListener('click', openPopup);
 popupCloseBtnElement.addEventListener('click', closePopup);
 popupElement.addEventListener('click', closePopupByClickOverlay);
 
-// редактирование формы
-const formElement = document.querySelector('.form');
-let nameInput = formElement.querySelector('.form__field');
-let jobInput = formElement.querySelector('.form__field');
-
 const handleFormSubmit = function (evt) {
   evt.preventDefault();
 
-  let nameInputNewValue = document.querySelector('.profile__title');
-  let jobInputNewValue = document.querySelector('.profile__subtitle');
   nameInputNewValue.textContent = nameInput.value;
   jobInputNewValue.textContent = jobInput.value;
-
   closePopup();
 }
 
