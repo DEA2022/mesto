3019bea79e2262cb6e7441a0793d0ae364bdc93e
Ekaterina Danilova <ekat.sokolova2010@yandex.ru>
2023-03-10 19:39:57 +0300

    feat: Сделать попап для добавления картинки и реализовать добавление картинки в начале массива

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 const popupElement = document.querySelector('.popup');
+const popupElementForAddImg = document.querySelector('.popup-img');
 const popupOpenBtnElement = document.querySelector('.profile__edit');
 const popupOpenForAddImgBtnElement = document.querySelector('.profile__button');
 const popupCloseBtnElement = popupElement.querySelector('.popup__close');
+const popupCloseForAddImgBtnElement = document.querySelector('.popup__close_add_img');
 const formElement = document.querySelector('.form');
+const formElementForAddCard = popupElementForAddImg.querySelector('.form');
 let nameInput = formElement.querySelector('.form__field_el_name');
 let jobInput = formElement.querySelector('.form__field_el_job');
 let nameInputNewValue = document.querySelector('.profile__title');
@@ -43,7 +46,8 @@ const initialCards = [
 const cardTemplate = document.querySelector('.card').content;
 const photoCard = document.querySelector('.photo__grid');
 
-initialCards.forEach(function(item) {
+
+initialCards.forEach(function (item) {
   const cardElement = cardTemplate.querySelector('.photo__card').cloneNode(true);
   cardElement.querySelector('.photo__item').src = item.link;
   cardElement.querySelector('.photo__item').alt = item.alt;
@@ -52,6 +56,11 @@ initialCards.forEach(function(item) {
   photoCard.append(cardElement);
 });
 
+let nameOfCard = document.querySelector('.photo__name');
+let srcOfCard = document.querySelector('.photo__item').src;
+let nameOfCardField = document.querySelector('.form__field_el_place');
+let srcOfCardField = document.querySelector('.form__field_el_webcite');
+
 const openPopup = function () {
   nameInput.value = nameInputNewValue.textContent;
   jobInput.value = jobInputNewValue.textContent;
@@ -59,37 +68,48 @@ const openPopup = function () {
 };
 
 const openPopupForAddImg = function () {
-
-  popupElement.classList.add('popup_opened');
+  popupElementForAddImg.classList.add('popup_opened');
 };
-
-
 
 const closePopup = function () {
   popupElement.classList.remove('popup_opened');
+};
 
+const closePopupForAddImg = function () {
+  popupElementForAddImg.classList.remove('popup_opened');
 };
 
 const closePopupByClickOverlay = function (event) {
   if (event.currentTarget === event.target) {
     closePopup();
+    closePopupForAddImg();
   }
-}
-
+};
+//для попапа профиля
 popupOpenBtnElement.addEventListener('click', openPopup);
 popupCloseBtnElement.addEventListener('click', closePopup);
 popupElement.addEventListener('click', closePopupByClickOverlay);
-
-popupOpenForAddImgBtnElement.addEventListener('click', openPopup);
+//для попапа добавления картинки
+popupOpenForAddImgBtnElement.addEventListener('click', openPopupForAddImg);
+popupCloseForAddImgBtnElement.addEventListener('click', closePopupForAddImg);
+popupElementForAddImg.addEventListener('click', closePopupByClickOverlay);
 
 const handleFormSubmit = function (evt) {
   evt.preventDefault();
-
   nameInputNewValue.textContent = nameInput.value;
   jobInputNewValue.textContent = jobInput.value;
   closePopup();
-}
+};
 
 formElement.addEventListener('submit', handleFormSubmit);
 
+const addNewCardFormSubmit = function (evt) {
+  evt.preventDefault();
+  const cardElement = cardTemplate.querySelector('.photo__card').cloneNode(true);
+  cardElement.querySelector('.photo__name').textContent = nameOfCardField.value;
+  cardElement.querySelector('.photo__item').src = srcOfCardField.value;
+  photoCard.prepend(cardElement);
+  closePopupForAddImg();
+};
 
+formElementForAddCard.addEventListener('submit', addNewCardFormSubmit);
