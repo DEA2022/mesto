6fb13a1850e5bba7174798b57f7c1c332b3f7a4c
Ekaterina Danilova <ekat.sokolova2010@yandex.ru>
2023-03-23 13:20:41 +0300

    fix: изменить фото и описания к ним в массиве

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,33 +1,33 @@
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-    alt: 'зеленые горы Архыза'
+    name: 'Париж',
+    link: 'https://images.unsplash.com/photo-1679231926690-cb1e8abfcefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1991&q=80',
+    alt: 'Эйфелева башня на фоне заката'
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-    alt: 'река со снежными берегами'
+    name: 'Венеция',
+    link: 'https://images.unsplash.com/photo-1677362376803-93af835b3a47?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    alt: 'Лодки, плывущие по улице Венеции'
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-    alt: 'типовые многоэтажки'
+    name: 'Египет',
+    link: 'https://images.unsplash.com/photo-1671483330965-357fcbb85999?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    alt: 'Пирамиды Гиза и сфинкс'
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-    alt: 'гора на фоне поля'
+    name: 'Индия',
+    link: 'https://images.unsplash.com/photo-1671219823638-9cd8bf0a80b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1012&q=80',
+    alt: 'Таджмахал в Агре'
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-    alt: 'железная дорога'
+    name: 'Лиссабон',
+    link: 'https://images.unsplash.com/photo-1665066358267-16768e4d46f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=694&q=80',
+    alt: 'Трамвай едет по узкой улочке Лиссабона'
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-    alt: 'берега Байкала'
+    name: 'Тайланд',
+    link: 'https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    alt: 'Зона отдыха возле бассейна'
   }
 ];
 
